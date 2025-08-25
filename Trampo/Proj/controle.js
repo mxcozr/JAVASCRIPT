78cd2195111ec12d995dde2cdc6b01d26536768c
mxcozr <mekko.acc@gmail.com>
2025-08-25 17:32:43 -0300

    LT2508

--- Trampo/Proj/controle.js
+++ Trampo/Proj/controle.js
@@ -11,10 +11,10 @@ function addTarefa(){
         ++contador; 
 
         let novoItem = `<div id="${(contador)}" class="item">
-            <div class="itemIcone">
-                <i class="mdi mdi-circle-outline"></i>
+            <div onclick="marcarTarefa(${contador})"class="itemIcone">
+                <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
             </div>
-            <div class="itemNome">
+            <div onclick="marcarTarefa(${contador})"class="itemNome">
                 ${valorInput}
             </div>
             <div class="itemBotao">
@@ -32,6 +32,28 @@ function addTarefa(){
 function deletar (id){
     var tarefa = document.getElementById(id);
     tarefa.remove();
+}
+
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    console.log (classe);
+
+    if(classe == "item"){
+        item.classList.add('clicado');
+
+        var icone=document.getElementById('icone_'+id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+
+        item.parentNode.appendChild(item);
+    }else{
+        item.classList.remove('clicado');
+
+        var icone=document.getElementById('icone_'+id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
 }
 
 input.addEventListener("keyup", function(event){
